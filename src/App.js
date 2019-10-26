2e0db6481b4ad7a5f6fc3a82ca8fe9c1889f0138
Sercan <s.selcuk23@gmail.com>
2019-10-26 23:20:21 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { currentCategory: "", products: [], cart: [] };
+
+  changeCategory = category => {
+    this.setState({ currentCategory: category.categoryName });
+    this.getProducts(category.id);
+  };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ products: data }));
+  };
+
+  addToCart = product => {
+    let newCart = this.state.cart;
+    var addedItem = newCart.find(c => c.product.id === product.id);
+
+    if (addedItem) {
+      addedItem.quatity += 1;
+    } else {
+      newCart.push({ product: product, quatity: 1 });
+    }
+
+    this.setState({ cart: newCart });
+  };
+
+  render() {
+    let productInfo = { title: "ProductList" };
+    let categoryInfo = { title: "CategoryList" };
+
+    return (
+      <div className="App">
+        <Container>
+          <Navi cart={this.state.cart}></Navi>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              ></CategoryList>
+            </Col>
+
+            <Col xs="9">
+              <ProductList
+                addToCart={this.addToCart}
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              ></ProductList>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
